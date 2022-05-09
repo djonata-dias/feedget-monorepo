513c4bfc76c7eab59d22b372853aa499513edf16
DJONATA DE SOUZA DIAS <djonata.dias@dotgroup.com.br>
2022-05-09 00:52:14 -0300

    solving a problem with cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,9 @@ import router from "./routes";
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(cors());
 
 app.listen(env.PORT, () => {
   console.log("Server started on port 3333");
